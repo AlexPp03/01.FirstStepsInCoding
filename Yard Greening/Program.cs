ebf89a7ec143d5e89b07fcc0970489b34ab54abd
AlexPp03 <156581032+AlexPp03@users.noreply.github.com>
2024-01-14 13:49:35 +0200

    Add files via upload

---
+++ Yard Greening/Program.cs
@@ -0,0 +1,11 @@
+﻿double yard = double.Parse(Console.ReadLine());
+
+double price = yard * 7.61;
+double discount = 0.18 * price;
+double finalprice = price - discount;
+
+Console.WriteLine($"The final price is: {finalprice} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
+
+
+
